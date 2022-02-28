d40de48365ba02b54d4aaff081a84c6bc4f30e35
Arun MR <arun_mr549e@protonmail.com>
2022-02-28 11:53:46 +0530

    Initial files

---
+++ email_sender/app.py
@@ -0,0 +1,26 @@
+import json
+
+import EmailSender as email_sender
+
+# import requests
+
+
+def lambda_handler(event, context):
+
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    email_sender.send_email({})
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
