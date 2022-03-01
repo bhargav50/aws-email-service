f5f3984b223bf6c389c859aa7b21b24248157cda
Arun MR <arun_mr549e@protonmail.com>
2022-03-01 12:36:41 +0530

    sam deploy for SQS done

--- email_sender/app.py
+++ email_sender/app.py
@@ -19,6 +19,22 @@ def lambda_handler(event, context):
     }
 
 
+def lambda_handler_sqs(event, context):
+    # Handling SQS request
+    for record in event['Records']:
+        payload = json.loads(record["body"])
+        print(payload)
+    email_sender.send_email(payload)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
+
+
 def get_email_dict(payload):
     email_dict = {}
     email_dict[""]
