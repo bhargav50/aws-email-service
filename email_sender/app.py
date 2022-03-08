a5b0cf85ceef333990854c7ef6ca7fb5d9d703d1
Arun MR <arun_mr549e@protonmail.com>
2022-03-08 11:33:50 +0530

    queue policy supported in template yml

--- email_sender/app.py
+++ email_sender/app.py
@@ -3,18 +3,9 @@ import json
 import EmailSender as email_sender
 import ESLogger as eslogger
 
-""" Handle messages from SQS """
+
+""" Handle messages from SQS"""
 def lambda_handler_sqs(event, context):
-    # Handling SQS request
-    for record in event['Records']:
-        payload = json.loads(record["body"])
-        print(payload)
-    response = email_sender.send_email(payload)
-
-    return response
-
-""" Handle messages from SQS routed through SNS """
-def lambda_handler_sns_sqs(event, context):
     eslogger.info("Received message from SQS --------")
     eslogger.info(event)
 
@@ -24,10 +15,6 @@ def lambda_handler_sns_sqs(event, context):
         payload = json.loads(record["body"])
         eslogger.info("payload as dict ----")
         eslogger.info(payload)
-        message = payload['Message']
-        eslogger.info("Message from SNS payload ----")
-        eslogger.info(message)
-        email_dict = json.loads(message)
-    response = email_sender.send_email(email_dict)
+    response = email_sender.send_email(payload)
 
     return response
