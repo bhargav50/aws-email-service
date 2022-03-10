917de8dabf536f7236dc09ea15bafa3ba7127539
Arun MR <arun_mr549e@protonmail.com>
2022-03-10 10:13:46 +0530

    Support added for batch processing

--- email_sender/app.py
+++ email_sender/app.py
@@ -6,15 +6,14 @@ import ESLogger as eslogger
 
 """ Handle messages from SQS"""
 def lambda_handler_sqs(event, context):
-    eslogger.info("Received message from SQS --------")
+    eslogger.info("Received message Batch from SQS --------")
     eslogger.info(event)
 
+    eslogger.info("Batch size " + str(len(event['Records'])))
     for record in event['Records']:
-        eslogger.info("Body of SQS payload ----")
+        eslogger.info("Body of Message ----" + record['messageId'])
         eslogger.info(record["body"])
         payload = json.loads(record["body"])
-        eslogger.info("payload as dict ----")
-        eslogger.info(payload)
-    response = email_sender.send_email(payload)
+        response = email_sender.send_email(payload)
 
     return response
