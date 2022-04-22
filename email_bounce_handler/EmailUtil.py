ada9d2b78478a616647485c2eca41cf864043942
Arun MR <arun_mr549e@protonmail.com>
2022-04-22 21:41:33 +0530

    Generic exception caught for bounce handler to avoid retries

--- email_bounce_handler/EmailUtil.py
+++ email_bounce_handler/EmailUtil.py
@@ -7,9 +7,13 @@ clientv2 = boto3.client('sesv2')
 def add_to_suppression_list(emails):
     if emails is not None:
         for email in emails:
-            response = clientv2.put_suppressed_destination(
-                EmailAddress=email,
-                Reason='BOUNCE'
-            )
-            eslogger.info('Adding ' + email + ' to suppression list')
-            eslogger.info(response)
+            try:
+                response = clientv2.put_suppressed_destination(
+                    EmailAddress=email,
+                    Reason='BOUNCE'
+                )
+                eslogger.info('Adding ' + email + ' to suppression list')
+                eslogger.info(response)
+            except Exception as e:
+                eslogger.info('Exception adding email ' + email + ' to suppression list')
+                eslogger.error(e)
